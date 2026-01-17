0c4d287f20a59d0b3525ad3ca6ab05659d751315
Sandeshth148 <sandeshth582@gmail.com>
2026-01-17 22:58:47 +0530

    Add NgRx Store providers to TasksComponent using importProvidersFrom for Module Federation compatibility

--- src/app/features/tasks/tasks.component.ts
+++ src/app/features/tasks/tasks.component.ts
@@ -1,11 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, importProvidersFrom } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EnhancedTaskTrackerComponent } from '../../components/enhanced-task-tracker/enhanced-task-tracker.component';
+import { taskReducer } from '../../store/task.reducer';
+import { TaskEffects } from '../../store/task.effects';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   imports: [CommonModule, EnhancedTaskTrackerComponent],
+  providers: [
+    importProvidersFrom(
+      StoreModule.forRoot({ tasks: taskReducer }),
+      EffectsModule.forRoot([TaskEffects]),
+      StoreDevtoolsModule.instrument({ maxAge: 25 })
+    )
+  ],
   template: `<app-enhanced-task-tracker></app-enhanced-task-tracker>`,
   styles: [`
     :host {
